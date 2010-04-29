7782e62c7ba7673e5358da83b692351b354ffe7d
Brian M. Clapper <bmc@clapper.org>
2010-04-29 16:55:46 -0400

    Checkpoint

---
+++ src/main/scala/org/clapper/classutil/ClassFinder.scala
@@ -0,0 +1,54 @@
+/*
+  ---------------------------------------------------------------------------
+  This software is released under a BSD license, adapted from
+  http://opensource.org/licenses/bsd-license.php
+
+  Copyright (c) 2010, Brian M. Clapper
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are
+  met:
+
+  * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+
+  * Neither the names "clapper.org", "Grizzled ClassUtil", nor the names of
+    its contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+  PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  ---------------------------------------------------------------------------
+*/
+
+package grizzled.classutil.classfinder
+
+import org.clapper.util.classutil._
+
+class ClassFilterBuilder(cf1: ClassFilter)
+{
+    def &(cf2: ClassFilter): AndClassFilter = new AndClassFilter(cf1, cf2)
+    def |(cf2: ClassFilter): OrClassFilter = new OrClassFilter(cf1, cf2)
+    def unary_! = new NotClassFilter(cf1)
+}
+
+object ClassFilter
+{
+    implicit def classFilterToBuilder(cf: ClassFilter): ClassFilterBuilder =
+        new ClassFilterBuilder(cf)
+}
+
