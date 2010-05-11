df83b3a6e314b394d212c6b5abe012ecb3415329
Brian M. Clapper <bmc@clapper.org>
2010-05-11 19:08:08 -0400

    Re-added publishing

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -69,6 +69,18 @@ class Project(info: ProjectInfo)
     val scalaVersionDir = "scala-" + buildScalaVersion
 
     /* ---------------------------------------------------------------------- *\
+                                Publishing
+    \* ---------------------------------------------------------------------- */
+
+    // "publish" will prompt (via a Swing pop-up) for the username and
+    // password.
+    lazy val publishTo = Resolver.sftp("clapper.org Maven Repo",
+                                       "maven.clapper.org",
+                                       "/var/www/maven.clapper.org/html")
+
+    override def managedStyle = ManagedStyle.Maven
+
+    /* ---------------------------------------------------------------------- *\
                        Managed External Dependencies
     \* ---------------------------------------------------------------------- */
 
